3b76fdb9199c1902a0ff001d04ee0fbc8c2529de
Rubens Lemos <snebur84@gmail.com>
2025-11-08 16:29:18 +0000

    Implementing OAuth and updating interfaces

--- provision/settings.py
+++ provision/settings.py
@@ -84,6 +84,9 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
